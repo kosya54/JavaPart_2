996f47efe3ba482d3f7db97f1ecc7523410db995
kosya54 <49726304+kosya54@users.noreply.github.com>
2019-07-15 13:57:54 +0300

    Update MainRange.java

--- Range/src/MainRange.java
+++ Range/src/MainRange.java
@@ -5,62 +5,66 @@ public class MainRange {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите начало первого интервала: ");
-        double fromA;
+        double from1;
         if (scanner.hasNextDouble()) {
-            fromA = scanner.nextDouble();
+            from1 = scanner.nextDouble();
         } else {
             System.out.println("Вы ввели не верные данные.");
             return;
         }
 
         System.out.println("Введите конец первого интервала: ");
-        double toA;
+        double to1;
         if (scanner.hasNextDouble()) {
-            toA = scanner.nextDouble();
+            to1 = scanner.nextDouble();
         } else {
             System.out.println("Вы ввели не верные данные.");
             return;
         }
 
-        Range rangeA = new Range(fromA, toA);
+        Range range1 = new Range(from1, to1);
 
         System.out.println("Введите начало второго интервала: ");
-        double fromB;
+        double from2;
         if (scanner.hasNextDouble()) {
-            fromB = scanner.nextDouble();
+            from2 = scanner.nextDouble();
         } else {
             System.out.println("Вы ввели не верные данные.");
             return;
         }
 
         System.out.println("Введите конец второго интервала: ");
-        double toB;
+        double to2;
         if (scanner.hasNextDouble()) {
-            toB = scanner.nextDouble();
+            to2 = scanner.nextDouble();
         } else {
             System.out.println("Вы ввели не верные данные.");
             return;
         }
 
-        Range rangeB = new Range(fromB, toB);
+        Range range2 = new Range(from2, to2);
 
-        System.out.printf("Интревал 1: {%.1f : %.1f}, длина интервала = %.0f.%n", rangeA.getFrom(), rangeA.getTo(), rangeA.getLength());
-        System.out.printf("Интервал 2: {%.1f : %.1f}, длина интервала = %.0f.%n", rangeB.getFrom(), rangeB.getTo(), rangeB.getLength());
-
-        Range rangeC = rangeA.getIntersection(rangeB);
-
-        if (rangeC == null) {
-            System.out.println("Интервалы не пересекаются.");
+        System.out.printf("Интревал 1: {%.1f : %.1f}, длина интервала = %.0f.%n", range1.getFrom(), range1.getTo(), range1.getLength());
+        System.out.printf("Интервал 2: {%.1f : %.1f}, длина интервала = %.0f.%n", range2.getFrom(), range2.getTo(), range2.getLength());
+        
+        if (range1.getIntersection(range2) == null) {
+            System.out.println("null");
         } else {
-            System.out.printf("Интервал пересечения: {%.1f : %.1f}, длина интервала = %.0f.%n", rangeC.getFrom(), rangeC.getTo(), rangeC.getLength());
+            System.out.printf("Интервал пересечения: {%.1f : %.1f}%n", range1.getIntersection(range2).getFrom(), range1.getIntersection(range2).getTo());
         }
-
-        Range[] rangeArray = rangeA.getUnion(rangeB);
-
-        if (rangeArray.length > 1) {
-            System.out.printf("Объединение интервалов равно: {%.1f : %.1f}, {%.1f : %.1f}", rangeArray[0].getFrom(), rangeArray[0].getTo(), rangeArray[1].getFrom(), rangeArray[1].getTo());
+        
+        if (range1.getUnion(range2).length == 1) {
+            System.out.printf("Интервал объединения: {%.1f : %.1f}%n", range1.getUnion(range2)[0].getFrom(), range1.getUnion(range2)[0].getTo());
         } else {
-            System.out.printf("Объединение интервалов равно: {%.1f : %.1f}", rangeArray[0].getFrom(), rangeArray[0].getTo());
+            System.out.printf("Интервалы объединения: {%.1f : %.1f}, {%.1f : %.1f}%n", range1.getUnion(range2)[0].getFrom(), range1.getUnion(range2)[0].getTo(), range1.getUnion(range2)[1].getFrom(), range1.getUnion(range2)[1].getTo());
+        }
+        
+        if (range1.getDifference(range2).length != 0) {
+            System.out.printf("Разность интервалов: {%.1f : %.1f}%n", range1.getDifference(range2)[0].getFrom(), range1.getDifference(range2)[0].getTo());
+        } else if (range1.getDifference(range2).length > 1) {
+            System.out.printf("Разность интервалов: {%.1f : %.1f}, {%.1f : %.1f}%n", range1.getDifference(range2)[0].getFrom(), range1.getDifference(range2)[0].getTo(), range1.getDifference(range2)[1].getFrom(), range1.getDifference(range2)[1].getTo());
+        } else {
+            System.out.println("Разность интервало: 0%n");
         }
     }
 }
