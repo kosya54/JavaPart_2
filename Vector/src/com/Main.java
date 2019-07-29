71211a59fcd9055f2d8b67619044b4bb44f08e59
kosya54 <kosya54@gmail.com>
2019-07-30 00:54:44 +0700

    Добавлена реализация некуоторых методов Vector

--- Vector/src/com/Main.java
+++ Vector/src/com/Main.java
@@ -4,15 +4,47 @@ import com.vector.Vector;
 
 class Main {
     public static void main(String[] args) {
+        double[] array = new double[] {1, 2, 5, 7, 9, 11, 15};
+
         Vector vector1 = new Vector(7);
-        System.out.println("Vector1: " + vector1);
-        System.out.println("Vector1Array: " + vector1.getVectorArray());
-        vector1.printVector();
+        System.out.println(vector1.toString());
+        System.out.println();
 
         Vector vector2 = new Vector(vector1);
-        System.out.println("Vector2: " + vector2);
-        System.out.println("Vector2Array: " + vector2.getVectorArray());
-        vector2.printVector();
-//        System.out.println("Вектор 1: " + vector1.getSize());
+        System.out.println(vector2.toString());
+        System.out.println();
+
+        Vector vector3 = new Vector(array);
+        System.out.println(vector3.toString());
+        System.out.println();
+
+
+        Vector vector4 = new Vector(5, array);
+        System.out.println("Vector4: " + vector4.toString());
+
+        Vector vector5 = new Vector(3, array);
+        System.out.println("Vector5: " + vector5.toString());
+        System.out.println();
+
+        vector4.getSum(vector5);
+        System.out.println("Vector4 + Vector 5 = " + vector4.toString());
+        System.out.println();
+
+        vector4.getSubtraction(vector5);
+        System.out.println("Vector4 - Vector 5 = " + vector4.toString());
+        System.out.println();
+
+        vector4.getMultiplication(2);
+        System.out.println("Vector4 * 2 = " + vector4.toString());
+        System.out.println();
+
+        vector4.getInverse();
+        System.out.println("Vector4 * -1 = " + vector4.toString());
+        System.out.println();
+
+        System.out.println("Vector4 length: " + vector4.getLength());
+        System.out.println();
+
+        System.out.println("Vector4 size: " + vector4.getSize());
     }
 }
--- Vector/src/com/vector/Vector.java
+++ Vector/src/com/vector/Vector.java
@@ -1,50 +1,100 @@
+//http://www.mathelp.spb.ru/book1/vector.htm
+
 package com.vector;
 
 import java.util.Arrays;
 
 public class Vector {
-    private int vectorSize;
-    private double[] vectorArray;
+    private int length;
+    private double[] vectorComponents;
 
     public Vector(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("n не должно быть <= 0!");
         }
 
-        vectorSize = n;
-        vectorArray = new double[vectorSize];
+        length = n;
+        vectorComponents = new double[length];
 
-        for (int i = 0; i < vectorSize; i++) {
-            vectorArray[i] = 0;
+        for (int i = 0; i < length; i++) {
+            vectorComponents[i] = 0;
         }
     }
 
-    public Vector(Vector otherVector) {
-        vectorSize = otherVector.vectorSize;
-        vectorArray = Arrays.copyOf(otherVector.vectorArray, otherVector.vectorArray.length);
+    public Vector(Vector vector) {
+        length = vector.length;
+        vectorComponents = Arrays.copyOf(vector.vectorComponents, vector.vectorComponents.length);
     }
 
-/*    Vector (double[] vector) {
-
+    public Vector(double[] array) {
+        vectorComponents = Arrays.copyOf(array, array.length);
     }
 
-    Vector(int n, double[] vector) {
+    public Vector(int n, double[] array) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n не должно быть <= 0!");
+        }
 
-    } */
+        length = n;
+        vectorComponents = Arrays.copyOf(array, length);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(vectorComponents).replace("[", "{ ").replace("]", " }");
+    }
+
+    public void getSum(Vector vector) {
+        if (length < vector.length) {
+            length = vector.length;
+            vectorComponents = Arrays.copyOf(vectorComponents, length);
+        }
+
+        if (length > vector.length) {
+            vector.vectorComponents = Arrays.copyOf(vector.vectorComponents, length);
+        }
+
+        for (int j = 0; j < length; j++) {
+            vectorComponents[j] += vector.vectorComponents[j];
+        }
+    }
+
+    public void getSubtraction(Vector vector) {
+        if (length < vector.length) {
+            length = vector.length;
+            vectorComponents = Arrays.copyOf(vectorComponents, length);
+        }
+
+        if (length > vector.length) {
+            vector.vectorComponents = Arrays.copyOf(vector.vectorComponents, length);
+        }
+
+        for (int j = 0; j < length; j++) {
+            vectorComponents[j] -= vector.vectorComponents[j];
+        }
+    }
+
+    public void getMultiplication(double number) {
+        for (int i = 0; i < length; i++) {
+            vectorComponents[i] *= number;
+        }
+    }
+
+    public void getInverse() {
+        final double INVERSE = -1;
+        getMultiplication(INVERSE);
+    }
+
+    public double getLength() {
+        double sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += Math.pow(vectorComponents[i], 2);
+        }
+        return Math.abs(Math.sqrt(sum));
+    }
 
     public int getSize() {
-        return vectorSize;
-    }
-
-    public double[] getVectorArray() {
-        return vectorArray;
-    }
-
-    public void setSize(int n) {
-        vectorSize = n;
-    }
-
-    public void printVector() {
-        System.out.println(Arrays.toString(vectorArray));
+        return length;
     }
 }
+
