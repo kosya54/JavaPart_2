d52193dfb0bb975679c031fc670d28eb53c2807b
kosya54 <kosya54@gmail.com>
2019-07-30 23:33:02 +0700

    Сделан Vector

--- Vector/src/com/Main.java
+++ Vector/src/com/Main.java
@@ -4,47 +4,67 @@ import com.vector.Vector;
 
 class Main {
     public static void main(String[] args) {
-        double[] array = new double[] {1, 2, 5, 7, 9, 11, 15};
+        System.out.println("-========Не статические методы========-");
+        System.out.println();
 
         Vector vector1 = new Vector(7);
-        System.out.println(vector1.toString());
+        System.out.println("Вектор1: " + vector1.toString());
+
+        double[] array = new double[]{1, 2, 5, 7, 9, 11, 15};
+
+        Vector vector2 = new Vector(array);
+        System.out.println("Вектор2: " + vector2.toString());
+
+        Vector vector3 = new Vector(vector2);
+        System.out.println("Вектор3: " + vector3.toString());
+
+
+        Vector vector4 = new Vector(20, array);
+        System.out.println("Вектор4: " + vector4.toString());
         System.out.println();
 
-        Vector vector2 = new Vector(vector1);
-        System.out.println(vector2.toString());
+
+        System.out.printf("Вектор1 == Вектор2 ? %b%nХэш Вектор1: %d%nХэш Вектор2: %d%n%n", vector1.equals(vector2), vector1.hashCode(), vector2.hashCode());
+        System.out.printf("Вектор2 == Вектор3 ? %b%nХэш Вектор2: %d%nХэш Вектор3: %d%n%n", vector2.equals(vector3), vector2.hashCode(), vector3.hashCode());
         System.out.println();
 
-        Vector vector3 = new Vector(array);
-        System.out.println(vector3.toString());
+        vector1.getSum(vector2);
+        System.out.printf("Вектор1 + Вектор2 = %s%n", vector1.toString());
+
+        vector4.getSubtraction(vector3);
+        System.out.printf("Вектор4 - Вектор3 = %s%n", vector4.toString());
+
+        vector2.getMultiplication(22);
+        System.out.printf("Вектор2 * 22 = %s%n", vector2.toString());
+
+        vector3.getInverse();
+        System.out.printf("Вектор3 * -1 = %s%n", vector3.toString());
         System.out.println();
 
-
-        Vector vector4 = new Vector(5, array);
-        System.out.println("Vector4: " + vector4.toString());
-
-        Vector vector5 = new Vector(3, array);
-        System.out.println("Vector5: " + vector5.toString());
+        System.out.printf("Длина Вектор1: %f, размер: %d%n", vector1.getLength(), vector1.getSize());
+        System.out.printf("Длина Вектор2: %f, размер: %d%n", vector2.getLength(), vector2.getSize());
+        System.out.printf("Длина Вектор3: %f, размер: %d%n", vector3.getLength(), vector3.getSize());
+        System.out.printf("Длина Вектор4: %f, размер: %d%n", vector4.getLength(), vector4.getSize());
         System.out.println();
 
-        vector4.getSum(vector5);
-        System.out.println("Vector4 + Vector 5 = " + vector4.toString());
+        System.out.printf("Компонента Вектор3 по индексу 5 равна: %f%n", vector3.getComponentByIndex(5));
+        System.out.println("Меняем компоненту Вектор3 по индексу 5 на 22.");
+        vector3.setComponentByIndex(5, 22);
+        System.out.printf("Компонента Вектор3 по индексу 5 равна: %f%n", vector3.getComponentByIndex(5));
         System.out.println();
 
-        vector4.getSubtraction(vector5);
-        System.out.println("Vector4 - Vector 5 = " + vector4.toString());
+        System.out.println("-========Статические методы========-");
         System.out.println();
 
-        vector4.getMultiplication(2);
-        System.out.println("Vector4 * 2 = " + vector4.toString());
+        Vector vector5 = Vector.getSum(vector1, vector2);
+        System.out.println("Вектор1: " + vector1.toString() + " + " + "Вектор2: " + vector2.toString() + " = " + "Вектор5: " + vector5.toString());
         System.out.println();
 
-        vector4.getInverse();
-        System.out.println("Vector4 * -1 = " + vector4.toString());
+        Vector vector6 = Vector.getSubtraction(vector5, vector3);
+        System.out.println("Вектор5: " + vector5.toString() + " - " + "Вектор3: " + vector3.toString() + " = " + "Вектор6: " + vector6.toString());
         System.out.println();
 
-        System.out.println("Vector4 length: " + vector4.getLength());
-        System.out.println();
-
-        System.out.println("Vector4 size: " + vector4.getSize());
+        Vector vector7 = new Vector(array);
+        System.out.printf("Вектор2 * Вектор7 = %f", Vector.getMultiplication(vector2, vector7));
     }
 }
--- Vector/src/com/vector/Vector.java
+++ Vector/src/com/vector/Vector.java
@@ -1,5 +1,3 @@
-//http://www.mathelp.spb.ru/book1/vector.htm
-
 package com.vector;
 
 import java.util.Arrays;
@@ -23,10 +21,11 @@ public class Vector {
 
     public Vector(Vector vector) {
         length = vector.length;
-        vectorComponents = Arrays.copyOf(vector.vectorComponents, vector.vectorComponents.length);
+        vectorComponents = Arrays.copyOf(vector.vectorComponents, length);
     }
 
     public Vector(double[] array) {
+        length = array.length;
         vectorComponents = Arrays.copyOf(array, array.length);
     }
 
@@ -42,6 +41,42 @@ public class Vector {
     @Override
     public String toString() {
         return Arrays.toString(vectorComponents).replace("[", "{ ").replace("]", " }");
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+
+        if (object == null || object.getClass() != getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) object;
+
+        if (length != vector.length) {
+            return false;
+        }
+
+        boolean isEqual = false;
+        for (int i = 0; i < length; i++) {
+            isEqual = vectorComponents[i] == vector.vectorComponents[i];
+        }
+        return isEqual;
+    }
+
+    @Override
+    public int hashCode() {
+        final int PRIME = 7;
+        int hash = 1;
+
+        hash = PRIME * hash + length;
+
+        for (int i = 0; i < length; i++) {
+            hash = PRIME * hash + Double.hashCode(vectorComponents[i]);
+        }
+        return hash;
     }
 
     public void getSum(Vector vector) {
@@ -66,6 +101,7 @@ public class Vector {
         }
 
         if (length > vector.length) {
+            vector.length = length;
             vector.vectorComponents = Arrays.copyOf(vector.vectorComponents, length);
         }
 
@@ -96,5 +132,82 @@ public class Vector {
     public int getSize() {
         return length;
     }
-}
 
+    public double getComponentByIndex(int index) {
+        try {
+            return vectorComponents[index];
+        } catch (ArrayIndexOutOfBoundsException error) {
+            index = 0;
+            System.out.printf("Индекс не может быть меньше 0! - %s, возвращаю компоненту с индексом %d.", error, index);
+        }
+        return vectorComponents[index];
+    }
+
+    public void setComponentByIndex(int index, double value) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Индекс не может быть меньше 0!");
+        }
+
+        if (index > length) {
+            throw new ArrayIndexOutOfBoundsException("Индекс не может быть больше длины компоненты.");
+        }
+        vectorComponents[index] = value;
+    }
+
+    public static Vector getSum(Vector vector1, Vector vector2) {
+        int length = vector1.length;
+
+        if (vector1.length < vector2.length) {
+            length = vector2.length;
+            vector1.vectorComponents = Arrays.copyOf(vector1.vectorComponents, vector2.length);
+        }
+
+        if (vector1.length > vector2.length) {
+            vector2.vectorComponents = Arrays.copyOf(vector2.vectorComponents, vector1.length);
+        }
+
+        double[] vectorComponents = new double[length];
+        for (int i = 0; i < length; i++) {
+            vectorComponents[i] = vector1.vectorComponents[i] + vector2.vectorComponents[i];
+        }
+        return new Vector(length, vectorComponents);
+    }
+
+    public static Vector getSubtraction(Vector vector1, Vector vector2) {
+        int length = vector1.length;
+
+        if (vector1.length < vector2.length) {
+            length = vector2.length;
+            vector1.vectorComponents = Arrays.copyOf(vector1.vectorComponents, vector2.length);
+        }
+
+        if (vector1.length > vector2.length) {
+            vector2.vectorComponents = Arrays.copyOf(vector2.vectorComponents, vector1.length);
+        }
+
+        double[] vectorComponents = new double[length];
+        for (int i = 0; i < length; i++) {
+            vectorComponents[i] = vector1.vectorComponents[i] - vector2.vectorComponents[i];
+        }
+        return new Vector(length, vectorComponents);
+    }
+
+    public static double getMultiplication(Vector vector1, Vector vector2) {
+        int length = vector1.length;
+
+        if (vector1.length < vector2.length) {
+            length = vector2.length;
+            vector1.vectorComponents = Arrays.copyOf(vector1.vectorComponents, vector2.length);
+        }
+
+        if (vector1.length > vector2.length) {
+            vector2.vectorComponents = Arrays.copyOf(vector2.vectorComponents, vector1.length);
+        }
+
+        double multiplicationResult = 0;
+        for (int i = 0; i < length; i++) {
+            multiplicationResult += vector1.vectorComponents[i] * vector2.vectorComponents[i];
+        }
+        return multiplicationResult;
+    }
+}
