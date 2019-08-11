d78bde5602a5ec2be96091b53fae2c8070c5406a
kosya54 <kosya54@gmail.com>
2019-08-11 22:45:34 +0700

    Добавлена задача ArrayListHome

--- ArrayListHome/src/com/ArrayListHome.java
+++ ArrayListHome/src/com/ArrayListHome.java
@@ -1,14 +1,14 @@
 package com;
 
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 class ArrayListHome {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("№1. Прочитать в список из файла.");
 
         ArrayList<String> strings = new ArrayList<>();
@@ -17,8 +17,10 @@ class ArrayListHome {
             while (scanner.hasNextLine()) {
                 strings.add(scanner.nextLine());
             }
+            System.out.println(strings);
+        } catch (FileNotFoundException error) {
+            System.out.println("Нет такого файла!" + error);
         }
-        System.out.println(strings);
         System.out.println();
 
         System.out.println("№2. Удаление четных чисел из списка.");
@@ -26,7 +28,11 @@ class ArrayListHome {
         ArrayList<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         System.out.println(numbers1);
 
-        numbers1.removeIf(number -> (number % 2 == 0));
+        for (int i = 0; i < numbers1.size(); i++) {
+            if (numbers1.get(i) != null && numbers1.get(i) % 2 == 0) {
+                numbers1.remove(numbers1.get(i));
+            }
+        }
 
         System.out.println(numbers1);
         System.out.println();
@@ -36,9 +42,9 @@ class ArrayListHome {
         ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5, 1, 3, 55, 1, 1, 1));
         System.out.println(numbers2);
 
-        ArrayList<Integer> numbers3 = new ArrayList<>();
+        ArrayList<Integer> numbers3 = new ArrayList<>(numbers2.size());
 
-        for (int value : numbers2) {
+        for (Integer value : numbers2) {
             if (!numbers3.contains(value)) {
                 numbers3.add(value);
             }
