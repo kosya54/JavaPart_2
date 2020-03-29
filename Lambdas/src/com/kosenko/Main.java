dd706955e2a997d1f957541a177c349889ecbab6
kosya54 <kosya54@gmail.com>
2020-03-29 16:35:31 +0700

    Сделана задача Лямбды

--- Lambdas/src/com/kosenko/Main.java
+++ Lambdas/src/com/kosenko/Main.java
@@ -19,25 +19,34 @@ public class Main {
         persons.add(new Person("Сири", 9));
 
         System.out.println("Пункты задачи А и Б:");
-        String uniqueNames = persons.stream().map(Person::getName).distinct()
+        String uniqueNames = persons.stream()
+                .map(Person::getName)
+                .distinct()
                 .collect(Collectors.joining(", ", "Имена: ", "."));
+
         System.out.println(uniqueNames);
         System.out.println();
 
         System.out.println("Пункт задачи В:");
-        persons.stream().filter(age -> age.getAge() < 18).mapToDouble(Person::getAge)
-                .average().ifPresent(System.out::println);
+        persons.stream()
+                .filter(age -> age.getAge() < 18)
+                .mapToDouble(Person::getAge)
+                .average()
+                .ifPresent(System.out::println);
         System.out.println();
 
         System.out.println("Пункт задачи Г:");
         Map<String, Double> personsMap = persons.stream()
                 .collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
+
         personsMap.forEach((name, averageAge) -> System.out.println(name + ": " + averageAge));
         System.out.println();
 
         System.out.println("Пункт задачи Д:");
-        persons.stream().filter(age -> age.getAge() >= 20 && age.getAge() <= 45)
+        persons.stream()
+                .filter(age -> age.getAge() >= 20 && age.getAge() <= 45)
                 .sorted(Comparator.comparingInt(Person::getAge).reversed())
-                .map(Person::getName).forEach(System.out::println);
+                .map(Person::getName)
+                .forEach(System.out::println);
     }
 }
